0f267c495ead1446ce84337707f960f9b1e9a1f9
vanemcb <vanem2201@gmail.com>
2022-12-08 18:41:05 -0500

    Task 6 fixed

--- 0x02-dart-classes/6-inheritance.dart
+++ 0x02-dart-classes/6-inheritance.dart
@@ -1,48 +1,34 @@
-// Update User class from 1-usertojson.dart
-// Add id property, fromJson method, and override toString method
-
 import '6-password.dart';
 
-class User extends Password{
-  int id;
+class User extends Password {
+  int? id;
   String name;
   int age;
   double height;
-  String user_password;
+  String? user_password;
 
-  // Constructor with parameters - must use required so not nullable
-  User({
-    required this.id,
-    required this.name,
-    required this.age,
-    required this.height,
-    required this.user_password,
-    // Calling Password constructor with Password password set to user_password
-  }) : super(password: user_password);
+  User(
+      {int? this.id,
+      required this.name,
+      required this.age,
+      required this.height,
+      String? this.user_password});
 
-  // Returns map representation of user
-  Map<String, dynamic> toJson() {
-    return {
-      "id": this.id,
-      "name": name,
-      "age": age,
-      "height": height,
-    };
+  Map toJson() {
+    return {"id": id, "name": name, "age": age, "height": height};
   }
 
-  // Returns string representation of user
-  // userJson is map/json representation of user - convert to string
   static User fromJson(Map<dynamic, dynamic> userJson) {
     return User(
-      id: userJson["id"],
-      name: userJson["name"],
-      age: userJson["age"],
-      height: userJson["height"],
-      user_password: userJson["user_password"],
-    );
+        id: userJson['id'],
+        name: userJson['name'],
+        age: userJson['age'],
+        height: userJson['height']);
   }
 
-  // Override default toString() method
-  @override
-  String toString() => "User(id : $id ,name: $name,  age: $age, height: $height, Password: ${isValid(user_password)}";
+  String toString() {
+    final pass = Password(password: user_password);
+
+    return "User(id : $id ,name: $name, age: $age, height: $height, Password: ${pass.isValid()})";
+  }
 }
--- 0x02-dart-classes/6-password.dart
+++ 0x02-dart-classes/6-password.dart
@@ -1,28 +1,25 @@
 class Password {
-  String _password = "";
+  String? _password = "";
 
-  // Different constructor syntax to instantiate private variables
-  Password({required String password}) : _password = password;
+  String get password => _password!;
 
-  // Getter method to access private variables
-  String get password => _password;
+  set password(String password) => _password = password;
+  Password({password}) : _password = password;
 
-  // Setter method to set private variables
-  set password(String pw) {
-    _password = pw;
+  bool isValid() {
+    if ((this._password?.length ?? 0) > 6 &&
+        (this._password?.length ?? 0) < 18) {
+      if (this._password!.contains(RegExp(r"(?=.*[a-z])(?=.*[A-Z])\w+"))) {
+        if (this._password!.contains(RegExp(r'\d'))) {
+          return true;
+        }
+      }
+    }
+    return false;
   }
 
-  bool isValid(userJson) {
-    // Password length between 8 and 16 characters long
-    return (this.password.length >= 8 && this.password.length <= 16) &&
-      // Password contains upper and lower case letters
-      this.password.contains(RegExp(r'[a-z]')) &&
-      this.password.contains(RegExp(r'[A-Z]')) &&
-      // Password contains at numbers
-      this.password.contains(RegExp(r'[0-9]'));
-  }
-
-  // toString() method already exists in Object class
   @override
-  String toString() => "Your Password is: ${this.password}";
+  String toString() {
+    return "Your Password is: ${this._password}";
+  }
 }
